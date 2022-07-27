effd57719d2c1fbbc46339a84885b7abd9c0a07a
Sven Kasaroczky <63825429+ZionGlitch@users.noreply.github.com>
2022-07-26 22:23:20 -0400

    Update madlib.py

--- Python Challenges/Python Mad Lib/madlib.py
+++ Python Challenges/Python Mad Lib/madlib.py
@@ -1,1 +1,8 @@
-
+def madlib1():
+    animal= input('enter an animal name: ')
+    grocery = input('enter a grocery item: ')
+    verb_past = input('enter verb in past tense: ')
+    verb_current = input('enter a verb in current tense: ')
+    item = input('enter a name of an item: ')
+    family_memb = input('enter a family member: ')
+    print('My '+family_memb+verb_past+' to the store to buy some '+grocery+' when he saw a giant '+animal+' in his way. He grabbed a nearby '+item+' and started '+verb_current+' it on the head until it ran away. He '+verb_past+' home right away to tell us all about it.')
